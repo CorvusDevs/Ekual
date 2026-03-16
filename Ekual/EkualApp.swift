7f9a5deb4203c4baff7b221187c00c2029ac4a78
Ale <soyelale@outlook.com>
2026-03-16 19:35:34 -0300

    Initial Commit

---
+++ Ekual/EkualApp.swift
@@ -0,0 +1,17 @@
+//
+//  EkualApp.swift
+//  Ekual
+//
+//  Created by Alejandro on 16/3/26.
+//
+
+import SwiftUI
+
+@main
+struct EkualApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
